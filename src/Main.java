ff8ceada05a37121f961f4cd33fc72d0be328e16
Mario Hercules <mariohercules@hotmail.com>
2019-03-07 10:17:52 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,25 @@ public class Main {
 
 		System.out.println("Bresenham Line Algorithm");
 
-		System.out.println("\nEntre com as dimensao da grade");
-		int rows = scan.nextInt();
-		int cols = scan.nextInt();
+		System.out.println("\nEntre com as dimensao da grade:");
+		int linhas = scan.nextInt();
+		int colunas = scan.nextInt();
 
-		Point[][] grid = new Point[rows][cols];
+		Point[][] grid = new Point[linhas][colunas];
 
-		for (int i = 0; i < rows; i++)
-			for (int j = 0; j < cols; j++)
+		for (int i = 0; i < linhas; i++)
+			for (int j = 0; j < colunas; j++)
 				grid[i][j] = new Point(i, j);
 
-		System.out.println("\nEntre com as coordinadas do ponto 1 e ponto 2");
-		int sr = scan.nextInt();
-		int sc = scan.nextInt();
-		int fr = scan.nextInt();
-		int fc = scan.nextInt();
+		System.out.println("\nEntre com as coordenadas do ponto 1 e ponto 2");
+		int x0 = scan.nextInt();
+		int y0 = scan.nextInt();
+		int xf = scan.nextInt();
+		int yf = scan.nextInt();
 
 		Bresenham b = new Bresenham();
 
-		List<Point> line = b.encontrarLinha(grid, sr, sc, fr, fc);
+		List<Point> line = b.encontrarLinha(grid, x0, y0, xf, yf);
 
 		b.desenharTela(grid, line);
 
